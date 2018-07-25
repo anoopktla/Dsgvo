d564d9af3fe1787a003d9ff48a45c2c675ad1053
Anoop K <anoop.krishnapillai@logicline.de>
2018-07-25 18:34:46 +0530

    dummy commit pdffix

--- src/main/java/de/logicline/adv/util/CustomerUtil.java
+++ src/main/java/de/logicline/adv/util/CustomerUtil.java
@@ -2,9 +2,12 @@ package de.logicline.adv.util;
 
 import de.logicline.adv.model.dao.AdvDao;
 import de.logicline.adv.model.dao.CustomerDao;
+import de.logicline.adv.model.dao.DataCategoryDao;
 import org.apache.commons.io.IOUtils;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class CustomerUtil {
@@ -16,13 +19,13 @@ public class CustomerUtil {
         customerDao.setSalutation("Mr");
         customerDao.setFirstName("Bibin");
         customerDao.setLastName("Paul");
-        customerDao.setPosition("VP");
+        customerDao.setPosition("Vice president");
         customerDao.setEmailAddress(id);
         customerDao.setPhoneNumber(944701887L);
         customerDao.setCompanyName("LogicLine");
-        customerDao.setStreet("W madison street");
-        customerDao.setBuildingNumber("500");
-        customerDao.setAddressLine2("chicago");
+        customerDao.setStreet("500 W madison street");
+        customerDao.setBuildingNumber("501");
+        customerDao.setAddressLine2("Chicago");
         customerDao.setZipCode("ec1a1hq");
         customerDao.setCity("Chicago");
         customerDao.setCountry("USA");
@@ -30,6 +33,20 @@ public class CustomerUtil {
         customerDao.setBcc("anoop.krishnapillai@heidelsoft.de");
         customerDao.setEmailTemplate("Test email body");
         AdvDao advDao = new AdvDao();
+        advDao.setValidFrom(new Date());
+        DataCategoryDao dataCategoryDao = new DataCategoryDao();
+        dataCategoryDao.setCategoryOfData("Non sensitive");
+        dataCategoryDao.setCategoryOfSubjects("category of subjects");
+        dataCategoryDao.setPurposeOfCollection("To investigate into the TPMS system");
+        List<DataCategoryDao> dataCategoryDaos = new ArrayList<>();
+        dataCategoryDaos.add(dataCategoryDao);
+        DataCategoryDao dataCategoryDao2 = new DataCategoryDao();
+        dataCategoryDao2.setCategoryOfData("Non PII");
+        dataCategoryDao2.setCategoryOfSubjects("category of subjects2");
+        dataCategoryDao2.setPurposeOfCollection("To troubleshoot the broken report generation");
+        dataCategoryDaos.add(dataCategoryDao2);
+
+        advDao.setDataCategoryDao(dataCategoryDaos);
         //advDao.setAdvInPdfFormat( IOUtils.toByteArray(pdfUtil.createPdf(customerDao)));
         List<AdvDao> advDaos = new ArrayList<>();
         advDaos.add(advDao);
--- src/main/java/de/logicline/adv/util/PdfManager.java
+++ src/main/java/de/logicline/adv/util/PdfManager.java
@@ -1,13 +1,11 @@
 package de.logicline.adv.util;
 
-import be.quodlibet.boxable.utils.PDStreamUtils;
 import de.logicline.adv.model.dao.CustomerDao;
+import de.logicline.adv.model.dao.DataCategoryDao;
 import org.apache.commons.io.IOUtils;
-import org.apache.fontbox.util.autodetect.FontFileFinder;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.core.io.Resource;
@@ -15,11 +13,17 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 
-import java.awt.*;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.net.URI;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class PdfManager {
@@ -29,6 +33,7 @@ public class PdfManager {
     private static final String PATH_PREFIX = "classpath:pdf";
     private static final String FILE_NAME = "Template_ADV_DE.pdf";
     private static final PDType1Font font = PDType1Font.HELVETICA;
+    private static final DateFormat DATE_FORMAT = DateFormat.getDateInstance(DateFormat.DEFAULT, Locale.GERMAN);
 
     @Autowired
     public PdfManager(MessageSource messageSource, ResourceLoader resourceLoader) {
@@ -41,7 +46,8 @@ public class PdfManager {
         InputStream fileAsInputStream = resource.getInputStream();
         PDDocument advAsPdfDocument = PDDocument.load(fileAsInputStream);
         fileAsInputStream.close();
-        PDPage firstPage = advAsPdfDocument.getPage(0);
+        /*PDPage firstPage = advAsPdfDocument.getPage(0);
+
         PDPageContentStream contentStream = new PDPageContentStream(advAsPdfDocument, firstPage,true,true);
         PDStreamUtils.write(contentStream, customerDao.getSalutation(), font,
                 9.0f, 240, 470, Color.BLACK);
@@ -50,11 +56,7 @@ public class PdfManager {
                 9.0f, 260, 470, Color.BLACK);
         PDStreamUtils.write(contentStream, customerDao.getLastName(), font,
                 9.0f, 290, 470, Color.BLACK);
-        FontFileFinder fontFinder = new FontFileFinder();
-        List<URI> fontURIs = fontFinder.find();
-        for(URI uri:fontURIs){
-            System.out.println(uri.toString());
-        }
+
 
         contentStream.close();
         PDPage thirdPage = advAsPdfDocument.getPage(2);
@@ -63,11 +65,66 @@ public class PdfManager {
                 9.0f, 160, 695, Color.BLACK);
         PDStreamUtils.write(contentStream, "1/1/1999", font,
                 9.0f, 240, 695, Color.BLACK);
-        contentStream.close();
+        contentStream.close(); ;
+*/
+
+
+        /* FontFileFinder fontFinder = new FontFileFinder();
+        List<URI> fontURIs = fontFinder.find();
+        for(URI uri:fontURIs){
+            System.out.println(uri.toString());
+        }*/
+        List<PDField> fields = advAsPdfDocument.getDocumentCatalog().getAcroForm().getFields();
+        Map<String, String> acroFormFiledmap = new HashMap<>();
+        Map<String, String> aggregatedStringMaps = getDataCategoryDetails(customerDao);
+        acroFormFiledmap.put("name", customerDao.getSalutation()+" "+customerDao.getFirstName()+" "+customerDao.getLastName());
+        acroFormFiledmap.put("name1", customerDao.getSalutation()+" "+customerDao.getFirstName()+" "+customerDao.getLastName());
+        acroFormFiledmap.put("address",customerDao.getCompanyName()+", "+customerDao.getStreet()+", "+customerDao.getBuildingNumber()+", "+customerDao.getCity()+", "+customerDao.getCountry());
+        acroFormFiledmap.put("date111",getFormattedDate(customerDao.getAdvDao().get(0).getValidFrom()));
+        acroFormFiledmap.put("date1", getFormattedDate(customerDao.getAdvDao().get(0).getValidFrom()));
+        acroFormFiledmap.put("date11",getFormattedDate(customerDao.getAdvDao().get(0).getValidFrom()));
+        acroFormFiledmap.put("designation",customerDao.getPosition());
+        acroFormFiledmap.put("designation1",customerDao.getPosition());
+        acroFormFiledmap.put("artUnd",aggregatedStringMaps.get("categoryOfData"));
+        acroFormFiledmap.put("dieDat",aggregatedStringMaps.get("categoryOfSubjects"));
+        acroFormFiledmap.put("dieVer",aggregatedStringMaps.get("purposeOfCollection"));
+        acroFormFiledmap.put("nameOfCompany","Logicline GmbH");
+        acroFormFiledmap.put("addressOfCompany","Planiestraße 10,71063, Sindelfingen, Germany");
+        for (PDField field : fields) {
+            for (Map.Entry<String, String> entry : acroFormFiledmap.entrySet()) {
+                if (entry.getKey().equals(field.getFullyQualifiedName())) {
+                    field.setValue(entry.getValue());
+                    field.setReadOnly(true);
+                }
+            }
+        }
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         advAsPdfDocument.save(out);
         advAsPdfDocument.close();
-        return out.toByteArray();
+
+        fileAsInputStream.close();
+        ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+
+        byte []  result  = IOUtils.toByteArray(in);
+        advAsPdfDocument.close();
+        return result;
+    }
+    private Map<String,String> getDataCategoryDetails(CustomerDao customerDao){
+
+        Map<String,String> mappings = new HashMap<>();
+        String lineSeparator = System.getProperty("line.separator");
+        String categoryOfData = customerDao.getAdvDao().get(0).getDataCategoryDao().stream().map(DataCategoryDao::getCategoryOfData).collect(Collectors.joining(lineSeparator));
+        String categoryOfSubjects = customerDao.getAdvDao().get(0).getDataCategoryDao().stream().map(DataCategoryDao::getCategoryOfSubjects).collect(Collectors.joining(lineSeparator));
+        String purposeOfCollection = customerDao.getAdvDao().get(0).getDataCategoryDao().stream().map(DataCategoryDao::getPurposeOfCollection).collect(Collectors.joining(lineSeparator));
+        mappings.put("categoryOfData",categoryOfData);
+        mappings.put("categoryOfSubjects",categoryOfSubjects);
+        mappings.put("purposeOfCollection",purposeOfCollection);
+        return mappings;
+    }
+
+    private String getFormattedDate(Date date){
+        return DATE_FORMAT.format(date);
+
     }
 
 }
